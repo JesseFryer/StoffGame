7a2452d8e6e1352b1defa4d8003646eab97fb84d
JesseFryer <jesse.fryer10@gmail.com>
2023-07-03 10:50:56 +1000

    Added Global struct for organisation, refactored UserInput into utils.

--- StoffGame/src/Application.cpp
+++ StoffGame/src/Application.cpp
@@ -1,12 +1,10 @@
-#include "renderer\Renderer.h"
-#include "UserInput.h"
+#include "renderer/Renderer.h"
+#include "utils/UserInput.h"
+#include "utils/Globals.h"
 
 #include <iostream>
 
-// Globals.
-float MOUSE_SCROLL = 0.0f;
-float MOUSE_SCROLL_SENSITIVITY = 0.2f;
-float CAMERA_SPEED = 2.0f;
+static Globals globals;
 
 void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
 
@@ -23,15 +21,14 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		userInput.UpdateInputs();
-
 		// Camera movement test with input class.
 		if (userInput.IsPressed(Key_ESC)) glfwSetWindowShouldClose(window, true);
-		if (userInput.IsPressed(Key_W)) renderer.MoveCamera(0.0f, CAMERA_SPEED);
-		if (userInput.IsPressed(Key_A)) renderer.MoveCamera(-CAMERA_SPEED, 0.0f);
-		if (userInput.IsPressed(Key_S)) renderer.MoveCamera(0.0f, -CAMERA_SPEED);
-		if (userInput.IsPressed(Key_D)) renderer.MoveCamera(CAMERA_SPEED, 0.0f);
-		renderer.ZoomCamera(MOUSE_SCROLL);
-		MOUSE_SCROLL *= 0.9f; // Slowly decreases scroll value for smooth zooming in/out.
+		if (userInput.IsPressed(Key_W)) renderer.MoveCamera(0.0f, globals.CAMERA_SPEED);
+		if (userInput.IsPressed(Key_A)) renderer.MoveCamera(-globals.CAMERA_SPEED, 0.0f);
+		if (userInput.IsPressed(Key_S)) renderer.MoveCamera(0.0f, -globals.CAMERA_SPEED);
+		if (userInput.IsPressed(Key_D)) renderer.MoveCamera(globals.CAMERA_SPEED, 0.0f);
+		renderer.ZoomCamera(globals.MOUSE_SCROLL);
+		globals.MOUSE_SCROLL *= 0.95f; // Slowly decreases scroll value for smooth zooming in/out.
 
 		// ------------------------ Render here ------------------------
 		glm::vec4 clearColour(0.2f, 0.6f, 0.8f, 1.0f);
@@ -52,5 +49,5 @@ int main()
 
 void ScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
 {
-	MOUSE_SCROLL = ((float) yOffset) * MOUSE_SCROLL_SENSITIVITY;
+	globals.MOUSE_SCROLL = ((float) yOffset) * globals.MOUSE_SCROLL_SENSITIVITY;
 }
---
+++ StoffGame/src/utils/Globals.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Globals
+{
+	float MOUSE_SCROLL = 0.0f;
+	const float MOUSE_SCROLL_SENSITIVITY = 0.2f;
+	const float CAMERA_SPEED = 2.0f;
+};
